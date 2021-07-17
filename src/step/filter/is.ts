d61f24e4588afd0a1fa58c19ae5101bd22f4c6b6
Benoit Simard <contact@bsimard.com>
2021-07-17 15:20:07 +0200

    Adding test on string for is step

---
+++ src/step/filter/is.ts
@@ -0,0 +1,15 @@
+import { isEqual } from "lodash";
+import { Traverser } from "../../type";
+import { GraphTraversal } from "../../traversal/graphTraversal";
+import { FilterStep } from "./generic";
+
+/**
+ * Filter the traverser if its element equals the specifiedvalue
+ */
+export class IsStep<T> extends FilterStep<T> {
+  constructor(traversal: GraphTraversal<unknown, T>, value: T) {
+    super("is", traversal, (traverser: Traverser<T>): boolean => {
+      return isEqual(traverser.value, value);
+    });
+  }
+}
